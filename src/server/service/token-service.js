5e965635ae76b91f9d113cb72c4a2c0912140924
CatWeb <uzuntudor@yahoo.com>
2021-09-20 12:05:47 +0300

    Registration validation working

--- src/server/service/token-service.js
+++ src/server/service/token-service.js
@@ -1,10 +1,13 @@
-import { verify } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
+import { model } from 'mongoose';
 import { TokenModel } from '../models/token-model';
 
 export const TokenService = {
   generateTokens: (payload) => {
-    const accessToken = verify(payload, 'jwt-secret-key', { expiresIn: '30m' });
-    const refreshToken = verify(payload, 'jwt-refresh-key', {
+    const accessToken = jwt.sign(payload, 'jwt-secret-key', {
+      expiresIn: '30m'
+    });
+    const refreshToken = jwt.sign(payload, 'jwt-refresh-key', {
       expiresIn: '30d'
     });
     return {
@@ -14,12 +17,13 @@ export const TokenService = {
   },
 
   saveToken: async (userId, refreshToken) => {
-    const tokenData = await TokenModel.findOne({ user: userId });
+    const TokenInfo = model('Token', TokenModel);
+    const tokenData = await TokenInfo.findOne({ user: userId });
     if (tokenData) {
       tokenData.refreshToken = refreshToken;
       return tokenData.save();
     }
-    const token = await TokenModel.create({ user: userId, refreshToken });
+    const token = await TokenInfo.create({ user: userId, refreshToken });
     return token;
   }
 };
--- src/server/service/user-service.js
+++ src/server/service/user-service.js
@@ -1,7 +1,7 @@
 import { model } from 'mongoose';
 import { UserModel } from '../models/user-model';
-import { bcrypt } from 'bcrypt';
-import { uuid } from 'uuid';
+import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
 import { MailService } from './mail-service';
 import { TokenService } from './token-service';
 
@@ -12,21 +12,20 @@ export const UserService = {
     if (candidate) {
       throw new Error(`User with this ${email} already exists`);
     }
-    const hashPassword = await bcrypt.hash(password, 3);
-    console.log(password, 'pass');
-    const activationLink = uuid.v4();
+    const hashPassword = bcrypt && (await bcrypt.hash(password, 3));
+    const activationLink = uuidv4();
     const user = await UserInfo.create({
-      id,
       email,
       password: hashPassword,
-      activationLink
+      activationLink: activationLink
     });
     await MailService.sendActivationMail(email, activationLink);
     const tokens = await TokenService.generateTokens({ ...user });
-    await tokenService.saveToken(user.id, tokens.refreshToken);
+    user && (await TokenService.saveToken(user.id, tokens.refreshToken));
 
     return {
-      ...tokens
+      ...tokens,
+      user
     };
   }
 };
