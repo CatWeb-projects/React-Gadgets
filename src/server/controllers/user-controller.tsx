e68aa804bbf945b59594f6e46720e497e5304319
CatWeb <uzuntudor@yahoo.com>
2021-09-22 11:02:35 +0300

    Login, Logout and Refresh Token features on backend added

--- src/server/controllers/user-controller.tsx
+++ src/server/controllers/user-controller.tsx
@@ -1,9 +1,17 @@
 import { UserService } from '../service/user-service';
+import { validationResult } from 'express-validator';
 
 export const UserController = {
   registration: async (request: any, response: any, next: any) => {
     try {
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        return Error('Registration is not valid');
+      }
       const { email, password } = request.body;
+      if (password === undefined) {
+        return request.cancel();
+      }
       const userData = await UserService.registration(email, password);
       response.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -16,12 +24,23 @@ export const UserController = {
   },
   login: async (request: any, response: any, next: any) => {
     try {
+      const { email, password } = request.body;
+      const userData = await UserService.login(email, password);
+      response.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+      });
+      return response.json(userData);
     } catch (e) {
       console.log(e);
     }
   },
   logout: async (request: any, response: any, next: any) => {
     try {
+      const { refreshToken } = request.cookies;
+      const token = await UserService.logout(refreshToken);
+      response.clearCookie('refreshToken');
+      return response.json(token);
     } catch (e) {
       console.log(e);
     }
@@ -34,7 +53,13 @@ export const UserController = {
   },
   refresh: async (request: any, response: any, next: any) => {
     try {
-      response.json(['123', 'this is sparta']);
+      const { refreshToken } = request.cookies;
+      const userData = await UserService.refresh(refreshToken);
+      response.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true
+      });
+      return response.json(userData);
     } catch (e) {
       console.log(e);
     }
